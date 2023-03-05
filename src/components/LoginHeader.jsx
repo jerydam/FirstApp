e003178f4f7894b2171c0bbdd81894487cf7eb9a
Jerydam <jerydan148@gmail.com>
2023-03-05 23:48:24 +0100

    first react app

---
+++ src/components/LoginHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginHeader = () => {
+  return (
+    <div class="bg-#1d4ed8">
+
+    </div>
+  )
+}
+
+export default LoginHeader
---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import LoginButton from './LoginButton'
+import LoginForm from './LoginForm'
+import LoginHeader from './LoginHeader'
+
+const LoginPage = () => {
+  return (
+    <div>
+  <LoginHeader/>
+  <LoginForm/>
+  <LoginButton/>
+    </div>
+  )
+}
+
+export default LoginPage
